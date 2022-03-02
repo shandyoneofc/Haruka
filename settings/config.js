465c84fa0690e8bfd003bb21b7007eb79d958b66
shandyoneofc <95661048+shandyoneofc@users.noreply.github.com>
2022-03-02 21:32:12 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // self or public
-global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+global.self = true //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownername ="SelenOfc"
+global.ownernumber = "6285600616877"
+global.botname = "Jinora-Bot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'RIFQIBOTZ' //apikey
